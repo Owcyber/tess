e0def37e85779d75e48b8c64cb101f63f82c182e
Owcyber <146743906+Owcyber@users.noreply.github.com>
2024-10-20 12:28:13 +0800

    Add files via upload

---
+++ portofolio/main.js
@@ -0,0 +1,30 @@
+const sections = document.querySelectorAll("section");
+const footers = document.querySelectorAll("footer");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("fade-in");
+        entry.target.style.visibility = "visible";
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
+
+footers.forEach((footer) => {
+  observer.observe(footer);
+});
+
+const hamburger = document.getElementById('hamburger');
+const navbar = document.getElementById('navbar');
+
+hamburger.addEventListener('click', function () {
+  const navUl = navbar.querySelector('ul');
+  navUl.classList.toggle('show');
+});
